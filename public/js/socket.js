43eabafec986258b9db0cdf4bd4497ec6afa2039
Daniela <daniela.arroyo27@gmail.com>
2020-03-06 14:14:02 -0600

    added js file

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,5 +1,16 @@
 let socket = io();
-
+//emit
+let form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let m = document.getElementById('m')
+    socket.emit('chat message', m.value);
+    return false;
+})
+//receive
+//msg is new broadcasted message from server
 socket.on('chat message', (msg) => {
-    console.log(msg)
+    let messages = document.getElementById('messages');
+    let liTag = messages.innerHTML + `<li>${msg}</li>`;
+    messages.innerHTML = liTag;
 })
